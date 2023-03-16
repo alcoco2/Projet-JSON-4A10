58161ca4bcda938831f11e02210fc146a69558a8
alcoco2 <107259105+alcoco2@users.noreply.github.com>
2023-03-16 16:45:39 +0100

    test Coco qui marche pas j'ai la haine

--- testCorentin.js
+++ testCorentin.js
@@ -1,13 +1,11 @@
+const countries = Country.all_countries;
 /**
  * Affiche les pays qui ont un frontalier hors de leur continent.
 */
 function outsideTheContinent(){
-    for(let country of countries){
-        for(let countryFrontalier of country.codeAlpha3.paysFrontaliers){
-            if (countryFrontalier = country.codeAlpha3) {
-                console.log(countryFrontalier)
-            }
-            console.log(country.codeAlpha3.paysFrontaliers)
-        }
+    for (let country of countries) {
+        let paysVoisin = country.codeAlpha3.getBorders();
+        console.log(paysVoisin.paysFrontalier)
     }
 }
+outsideTheContinent();
