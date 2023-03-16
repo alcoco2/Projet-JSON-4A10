862c315432353b0ff9cc80df2862b5257b28d38c
alcoco2 <107259105+alcoco2@users.noreply.github.com>
2023-03-16 15:17:48 +0100

    Test Corentin

--- testCorentin.js
+++ testCorentin.js
@@ -1,5 +1,13 @@
+/**
+ * Affiche les pays qui ont un frontalier hors de leur continent.
+*/
 function outsideTheContinent(){
     for(let country of countries){
-        console.log(country.codeAlpha3.paysFrontaliers)
+        for(let countryFrontalier of country.codeAlpha3.paysFrontaliers){
+            if (countryFrontalier = country.codeAlpha3) {
+                console.log(countryFrontalier)
+            }
+            console.log(country.codeAlpha3.paysFrontaliers)
+        }
     }
 }
