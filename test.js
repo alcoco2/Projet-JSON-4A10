87da57723b90599ac293531cfac29694cd4d4210
TigrouForMatic <jules.chassany@gmail.com>
2023-03-17 17:40:21 +0100

    Feature: test 9 --> Bug antartique a corrigé

--- test.js
+++ test.js
@@ -92,3 +92,142 @@ function moreLanguages(){
     }
     return paysAveclanguesNombreuses, paysAveclanguesNombreusesNom
 }
+
+
+/**
+ * Pays avec au moins un voisin ayant la meme langue. bi-check
+ */
+function withCommonLanguage() {
+    let countriesWithLanguage = [];
+
+    for (let country of countries) {
+        let hasCommonLanguage = false;
+
+        for (let neighbor of country.codeAlpha3.getBorders()) {
+            for (let neighborLanguage of neighbor.codeAlpha3.languages) {
+                for (let countryLanguage of country.codeAlpha3.getLanguages()) {
+                    if (neighborLanguage.iso639_2 === countryLanguage.iso639_2) {
+                        hasCommonLanguage = true;
+                        break;
+                    }
+                }
+
+                if (hasCommonLanguage) {
+                    break;
+                }
+            }
+
+            if (hasCommonLanguage) {
+                break;
+            }
+        }
+
+        if (hasCommonLanguage) {
+            countriesWithLanguage.push(country);
+        }
+    }
+
+    return countriesWithLanguage;
+}
+
+
+
+/**
+ * Pays sans aucun voisin ayant au moins une de ses monnaies. bi-check
+ */
+function withoutCommonCurrency() {
+    let countriesWithSameCurrency = [];
+    
+    for (let country of countries) {
+        let tabCommonCurrency = []
+        for (let neighbor of country.codeAlpha3.getBorders()) {
+            if (neighbor.codeAlpha3.currencies.length) {
+                for (let cuurency of neighbor.codeAlpha3.currencies) {
+                    let commonCurrency = country.codeAlpha3.getCurrencies().filter(curr => curr.code !== cuurency.code)
+                    tabCommonCurrency.push(commonCurrency)
+                }
+            }
+        }
+        if (!tabCommonCurrency.length) {
+            countriesWithSameCurrency.push(country);
+        }
+    }
+
+    return countriesWithSameCurrency.filter((x, i) => countriesWithSameCurrency.indexOf(x) === i);
+}
+
+/**
+ * Pays triés par ordre décroissant de densité de population. bi-check
+*/
+function sortingDecreasingDensity() {
+    let sortCountries = Country.all_countries;
+    sortCountries.sort((a, b) => {
+      return b.codeAlpha3.getPopDensity() - a.codeAlpha3.getPopDensity();
+    });
+    console.log(sortCountries)
+    return sortCountries;
+}
+
+/**
+ * Pays ayant plusieurs Top Level Domains Internet. bi-check
+ */
+function moreTopLevelDomains() {
+    let countryMoreTDL = [];
+    for(let country of countries){
+        let tld = country.codeAlpha3.tld;
+        if (tld.length > 1) {
+            countryMoreTDL.push(country.codeAlpha3);
+        }
+    }
+    return countryMoreTDL
+}
+
+// -> antartique 1
+
+/**
+ * Recherche et retourne l'objet Country a partir du nom ou du code du pays
+ * 
+ * @param {String} info 
+ * @param {String} type 
+ * @returns {Objet} Country
+ */
+function getPays(info, type){
+    for (let country of countries) {
+        if (type == "code") {
+            if (country.codeAlpha3.codeAlpha3 == info) {
+                return country.codeAlpha3
+            }
+        }
+        if (type == "nom") {
+            if (country.codeAlpha3.nom == info) {
+                return country.codeAlpha3
+            }
+        }
+
+    }
+}
+
+/**
+ * Retourne tout les pays voisins du pays entrer en parametre et iterativement avec tous les pays voisins
+ * @param {String} nom_pays 
+ * @returns 
+ */
+function veryLongTrip(nom_pays, frontalier = []) {
+    let pays = getPays(nom_pays, "nom");
+
+    if (Object.keys(pays.paysFrontaliers)) {
+        for (let neighbor of pays.paysFrontaliers) {
+          let nameNeighbor = getPays(neighbor, "code");
+          if (!frontalier.includes(nameNeighbor.nom)) {
+            frontalier.push(nameNeighbor.nom);
+            if (typeof nameNeighbor.paysFrontaliers != 'undefined'){
+                veryLongTrip(nameNeighbor.nom, frontalier);
+            }
+          }
+        }
+    }
+  
+    return frontalier;
+  }
+
+console.log(veryLongTrip("Antarctica"))
